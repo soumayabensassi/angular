afd20112300af8afd6e6986aa7ce635267d26f3a
soumayabensassi <soumaya.bensassi@esprit.tn>
2022-11-14 14:56:01 +0100

    search-filtre-map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ProductsComponent } from './products/products.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OffreemploiComponent } from './offreemploi/offreemploi.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { OffreemploiComponent } from './offreemploi/offreemploi.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/offreemploi/offreemploi.component.ts
+++ src/app/offreemploi/offreemploi.component.ts
@@ -9,13 +9,48 @@ import { Emploi } from '../core/article';
 export class OffreemploiComponent implements OnInit {
   title:string="welcome to article page!"
   emploitList!:Emploi[];
+  nombre:number=0;
+  textsearch:string="";
+  test:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
     this.emploitList=[
     {reference:"ref1",titre:"emploi1",entreprise:"entreprise1",etat:true},
-    {reference:"ref2",titre:"emploi2",entreprise:"entreprise2",etat:false}
+    {reference:"ref2",titre:"emploi2",entreprise:"entreprise2",etat:false},
+    {reference:"ref3",titre:"emploi3",entreprise:"entreprise3",etat:true}
+
   ];
   }
+  Bilan()
+  {this.nombre=0;
+    this.test=true;
+  
+    for(let i=0;i<this.emploitList.length;i++)
+{
+  if(this.emploitList[i].etat==true)
+  {
+this.nombre++;
+  }
+}
+  }
+  Bilan2()
+  {this.nombre=0;
+    this.test=true;
+    this.emploitList.map((x,i)=>
+    {
+      x.etat&&this.nombre++
+      return this.nombre;
+    })
+  
+  }
+  Search()
+  {
+    this.emploitList=this.emploitList.filter((x)=>
+    { 
+      return x.entreprise.match(this.textsearch)
+     })
+  }
+
 
 }
