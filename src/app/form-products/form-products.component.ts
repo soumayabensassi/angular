0fb1f7c41a6cbae4b2c13833d525cd49891d6a56
soumayabensassi <soumaya.bensassi@esprit.tn>
2022-11-24 15:32:11 +0100

    forms

--- src/app/form-products/form-products.component.ts
+++ src/app/form-products/form-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../core/products';
 
 @Component({
   selector: 'app-form-products',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-products.component.css']
 })
 export class FormProductsComponent implements OnInit {
-
+product:Product=new Product();
   constructor() { }
 
   ngOnInit(): void {
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+id!:number;
+  constructor(private v:ActivatedRoute) { 
+  }
 
   ngOnInit(): void {
+    this.id=this.v.snapshot.params['id']
   }
 
 }
